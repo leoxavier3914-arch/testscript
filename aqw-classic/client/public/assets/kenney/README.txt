7b80f747ac019a84f5593dc11a9b7c8faea269d0
leoxavier3914-arch <leoxavier3914@gmail.com>
2025-10-06 06:52:31 -0300

    Load Kenney sprites from public assets directory

---
+++ aqw-classic/client/public/assets/kenney/README.txt
@@ -0,0 +1,8 @@
+Coloque neste diretório os sprites extraídos do pack "Kenney Isometric Miniature Dungeon".
+Arquivos esperados pelo cliente:
+- player.png
+- monster.png
+- drop.png
+- ground-tile.png
+
+Os nomes devem corresponder exatamente aos listados acima e permanecer junto ao arquivo de licença (LICENSE.txt).
--- aqw-classic/client/src/scenes/BootScene.ts
+++ aqw-classic/client/src/scenes/BootScene.ts
@@ -1,39 +1,19 @@
 import Phaser from "phaser";
 
+const ASSET_BASE_PATH = "assets/kenney";
+
 export default class BootScene extends Phaser.Scene {
   constructor() {
     super("BootScene");
   }
 
   preload() {
-    const graphics = this.make.graphics({ x: 0, y: 0, add: false });
-
-    // Player placeholder (circle)
-    graphics.fillStyle(0x4caf50, 1);
-    graphics.fillCircle(32, 32, 28);
-    graphics.lineStyle(2, 0xffffff);
-    graphics.strokeCircle(32, 32, 28);
-    graphics.generateTexture("player", 64, 64);
-    graphics.clear();
-
-    // Monster placeholder (square)
-    graphics.fillStyle(0xe53935, 1);
-    graphics.fillRect(0, 0, 64, 64);
-    graphics.lineStyle(2, 0xffffff);
-    graphics.strokeRect(0, 0, 64, 64);
-    graphics.generateTexture("monster", 64, 64);
-    graphics.clear();
-
-    // Drop placeholder (triangle)
-    graphics.fillStyle(0xffd54f, 1);
-    graphics.beginPath();
-    graphics.moveTo(32, 0);
-    graphics.lineTo(0, 64);
-    graphics.lineTo(64, 64);
-    graphics.closePath();
-    graphics.fillPath();
-    graphics.generateTexture("drop", 64, 64);
-    graphics.destroy();
+    this.load.setPath(ASSET_BASE_PATH);
+    this.load.image("player", "player.png");
+    this.load.image("monster", "monster.png");
+    this.load.image("drop", "drop.png");
+    this.load.image("ground-tile", "ground-tile.png");
+    this.load.resetPath();
   }
 
   create() {
