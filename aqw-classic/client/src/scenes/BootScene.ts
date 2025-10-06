b6b4e2f95d66025a2c66a1342c80d23309a15f56
leoxavier3914-arch <leoxavier3914@gmail.com>
2025-10-06 07:36:56 -0300

    Remove versioned sprites and document manual assets

--- aqw-classic/client/src/scenes/BootScene.ts
+++ aqw-classic/client/src/scenes/BootScene.ts
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 
-const ASSET_BASE_PATH = "assets/kenney";
+const ASSET_BASE_PATH = "assets";
 
 export default class BootScene extends Phaser.Scene {
   constructor() {
@@ -9,7 +9,10 @@ export default class BootScene extends Phaser.Scene {
 
   preload() {
     this.load.setPath(ASSET_BASE_PATH);
-    this.load.image("player", "player.png");
+    this.load.spritesheet("player", "char.png", {
+      frameWidth: 256,
+      frameHeight: 256
+    });
     this.load.image("monster", "monster.png");
     this.load.image("drop", "drop.png");
     this.load.image("ground-tile", "ground-tile.png");
@@ -17,6 +20,41 @@ export default class BootScene extends Phaser.Scene {
   }
 
   create() {
+    this.createPlayerAnimations();
     this.scene.start("HubScene");
+  }
+
+  private createPlayerAnimations() {
+    if (this.anims.exists("player-idle")) {
+      return;
+    }
+
+    this.anims.create({
+      key: "player-idle",
+      frames: [{ key: "player", frame: 0 }],
+      frameRate: 1,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "player-run",
+      frames: this.anims.generateFrameNumbers("player", { frames: [0, 1, 2] }),
+      frameRate: 8,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "player-attack",
+      frames: this.anims.generateFrameNumbers("player", { frames: [3, 4, 5, 6] }),
+      frameRate: 12,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: "player-pickup",
+      frames: this.anims.generateFrameNumbers("player", { frames: [7, 8, 9, 10, 11] }),
+      frameRate: 10,
+      repeat: 0
+    });
   }
 }
